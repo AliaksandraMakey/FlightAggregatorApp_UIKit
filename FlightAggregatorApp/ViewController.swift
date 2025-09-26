eca1cc78a8c1a85715a0807da71cdf483a5f4833
AliaksandraMakey <aliaksandra.makey@gmail.com>
2025-09-26 14:27:49 +0400

    Initial Commit

---
+++ FlightAggregatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlightAggregatorApp
+//
+//  Created by Alexandra Makey on 26.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
